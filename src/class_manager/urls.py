b744ba6ef8cefbc9b48e50139b71638cc8ff0b7c
sot-andreas <icsd18215@icsd.aegean.gr>
2021-07-30 00:39:25 +0300

    Added teacher signup, login, home. Removed admin.

--- src/class_manager/urls.py
+++ src/class_manager/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
 import users.views
 
 urlpatterns = [
+    path("", users.views.signup_student, name="home"),
+    path("login", users.views.user_login, name="login"),
     path("signup/student", users.views.signup_student, name="signup_student"),
-    path('admin/', admin.site.urls),
+    path("signup/teacher", users.views.signup_teacher, name="signup_teacher")
 ]
