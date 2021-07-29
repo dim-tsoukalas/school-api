f2eb56d90fbc12611d852a714fe33cfd23d25444
sot-andreas <icsd18215@icsd.aegean.gr>
2021-07-29 20:55:04 +0300

    Added a url to /signup/students

--- src/class_manager/urls.py
+++ src/class_manager/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import users.views
+
 urlpatterns = [
+    path("signup/student", users.views.signup_student, name="signup_student"),
     path('admin/', admin.site.urls),
 ]
