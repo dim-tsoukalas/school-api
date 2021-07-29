dc376a843a6c70cf2e1516e8cf10e33c6c1685b4
sot-andreas <icsd18215@icsd.aegean.gr>
2021-07-29 20:54:25 +0300

    Added the users and mainpage apps. Added the new AUTH_USER_MODEL and SETTINGS_PATH.

--- src/class_manager/settings.py
+++ src/class_manager/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # User provided
+    "mainpage",
+    "users"
 ]
 
 MIDDLEWARE = [
@@ -80,6 +87,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
