5f9bf527e1d56fdbc1836f45ca0fcd2a0d1b86ba
sot-andreas <icsd18215@icsd.aegean.gr>
2021-07-30 22:24:41 +0300

    Pass the signin form to the template.

--- src/mainpage/views.py
+++ src/mainpage/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from users.forms import SigninForm
+
 
 def home(request):
-    return render(request, "mainpage.html", {"user": request.user})
+    r_dict = {"user": request.user}
+    if not request.user.is_authenticated:
+        r_dict["signin_form"] = SigninForm()
+    return render(request, "mainpage.html", r_dict)
