5c7c061a447e7d5415ab1e48c3f13dd902fb812f
sot-andreas <icsd18215@icsd.aegean.gr>
2021-08-04 00:30:22 +0300

    Spacing.

--- src/mainpage/views.py
+++ src/mainpage/views.py
@@ -7,4 +7,5 @@ def home(request):
     r_dict = {"user": request.user}
     if not request.user.is_authenticated:
         r_dict["signin_form"] = SigninForm()
+
     return render(request, "mainpage.html", r_dict)
