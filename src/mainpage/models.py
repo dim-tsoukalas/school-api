9f73ea551f1d8aaa6ff5558341e7a6da050ae0d0
sot-andreas <icsd18215@icsd.aegean.gr>
2021-08-03 18:29:59 +0300

    Make department names unique

--- src/mainpage/models.py
+++ src/mainpage/models.py
@@ -3,14 +3,19 @@ from django.db import models
 
 class Department(models.Model):
     NAME_LENGTH = 200
-    name = models.CharField(max_length=NAME_LENGTH)
+    name = models.CharField(max_length=NAME_LENGTH, unique=True)
+
+    def get_department_names():
+        return Department.objects.only("name")
 
 
 class DepartmentStudents(models.Model):
-    dept_id = models.ForeignKey("mainpage.Department", on_delete=models.CASCADE)
+    dept_id = models.ForeignKey(
+        "mainpage.Department", on_delete=models.CASCADE)
     student_id = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
 
 class DepartmentTeachers(models.Model):
-    dept_id = models.ForeignKey("mainpage.Department", on_delete=models.CASCADE)
+    dept_id = models.ForeignKey(
+        "mainpage.Department", on_delete=models.CASCADE)
     teacher_id = models.ForeignKey("users.User", on_delete=models.CASCADE)
