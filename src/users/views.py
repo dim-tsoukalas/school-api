dd25ff2feda2ebe2366bbde810a41df8e775ad7d
sot-andreas <icsd18215@icsd.aegean.gr>
2021-08-03 18:42:45 +0300

    Authenticated users are redirected to the homepage when attempting to access the signup page. Fixed errors in the signup functions: The users are no longer authenticated after signup (because it would make no sense, since their accounts are not enabled yet). The necessary values are provided to the template on successful signup to show the error message. The code has been simplified.

--- src/users/views.py
+++ src/users/views.py
@@ -1,62 +1,58 @@
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
 
 from .forms import StudentSignupForm, TeacherSignupForm, SigninForm
 
 
 def signup_student(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
     if request.method == "POST":
         form = StudentSignupForm(request.POST)
+        t_dict = {"signin_form": SigninForm(),
+                  "teacher_form": TeacherSignupForm(),
+                  "student_form": form}
         if form.is_valid():
-            user = form.save()
-            user.is_active = False  # The user must be activated by the admin
-            user.save()
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.email, password=password)
-            login(request, user)
-            return redirect("")
+            form.save()
+            t_dict["user_created"] = True
+            t_dict["user_first_name"] = form.cleaned_data.get("first_name")
+            t_dict["student_form"] = StudentSignupForm()
+
+        return render(request, "signup.html", t_dict)
     else:
         return redirect("signup")
-
-    t_dict = {"teacher_form": TeacherSignupForm(),
-              "student_form": form}
-
-    if not request.user.is_authenticated:
-        t_dict["signin_form"] = SigninForm()
-
-    return render(request, "signup.html", t_dict)
 
 
 def signup_teacher(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
     if request.method == "POST":
         form = TeacherSignupForm(request.POST)
+        t_dict = {"signin_form": SigninForm(),
+                  "teacher_form": form,
+                  "student_form": StudentSignupForm()}
         if form.is_valid():
-            user = form.save()
-            user.is_active = False  # The user must be activated by the admin
-            user.save()
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.email, password=password)
-            login(request, user)
-            return redirect("")
+            form.save()
+            t_dict["user_created"] = True
+            t_dict["user_first_name"] = form.cleaned_data.get("first_name")
+            t_dict["teacher_form"] = TeacherSignupForm()
+
+        return render(request, "signup.html", t_dict)
     else:
         return redirect("signup")
 
-    t_dict = {"teacher_form": form,
-              "student_form": StudentSignupForm()}
-
-    if not request.user.is_authenticated:
-        t_dict["signin_form"] = SigninForm()
-
-    return render(request, "signup.html", t_dict)
-
 
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
     t_dict = {
+        "signin_form": SigninForm(),
         "teacher_form": TeacherSignupForm(),
         "student_form": StudentSignupForm()
     }
-    if not request.user.is_authenticated:
-        t_dict["signin_form"] = SigninForm()
 
     return render(request, "signup.html", t_dict)
 
