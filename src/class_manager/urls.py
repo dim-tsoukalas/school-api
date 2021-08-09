18aa82a33d0d91f21bdf0182610a4904c56693c5
sot-andreas <icsd18215@icsd.aegean.gr>
2021-08-09 15:06:23 +0300

    Added user management urls for the users app. Removed the ones for the staff app.

--- src/class_manager/urls.py
+++ src/class_manager/urls.py
@@ -26,6 +26,10 @@ urlpatterns = [
     path("signup", users.views.signup, name="signup"),
     path("signup/student", users.views.signup_student, name="signup_student"),
     path("signup/teacher", users.views.signup_teacher, name="signup_teacher"),
+    # Users
+    path("users", users.views.users, name="users"),
+    path("users/<int:uid>", users.views.user),
+    path("users/<int:uid>/update", users.views.user_update),
     # Superuser
     path("superuser", staff.views.superuser, name="superuser"),
     path(
@@ -38,8 +42,4 @@ urlpatterns = [
         staff.views.superuser_departments_add,
         name="superuser_departments_add",
     ),
-    path(
-        "superuser/users", staff.views.superuser_users,
-        name="superuser_users"
-    )
 ]
