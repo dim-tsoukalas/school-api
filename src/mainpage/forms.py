d62ca687b915b745653e2b49b706cb8bfe94fe50
sot-andreas <icsd18215@icsd.aegean.gr>
2021-08-13 21:52:13 +0300

    CRUD interface for the departments. Based on icsd18232's commits. Updated to the style of the classes app.

--- src/mainpage/forms.py
+++ src/mainpage/forms.py
@@ -1,6 +1,25 @@
 from django import forms
 
+from mainpage.models import Department
+
+
+# ====================================================================
+# Fields
+# ====================================================================
 
 class DepartmentChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, obj):
         return obj.name
+
+
+# ====================================================================
+# Forms
+# ====================================================================
+
+class DepartmentAddForm(forms.ModelForm):
+    class Meta:
+        model = Department
+        fields = ["name", "description"]
+        widgets = {
+            "description": forms.Textarea()
+        }
