fb5c2c0bd259c040ae9ab483436f9f54d7fc24e3
sot-andreas <icsd18215@icsd.aegean.gr>
2021-08-15 20:51:46 +0300

    Added a form for only editing a Class' description (meant to be used by Teacher user types). Added a form for adding prerequisite classes. Added a clean method in the Teaching form to enforce the needed constraints.

--- src/classes/forms.py
+++ src/classes/forms.py
@@ -1,15 +1,24 @@
+from decimal import Decimal
+
 from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
-from django.db import transaction
 
-from mainpage.models import Department, DepartmentTeachers
-from mainpage.forms import DepartmentChoiceField
+from mainpage.models import DepartmentTeachers
 
 from users.models import Teacher
 from users.forms import TeacherChoiceField
 
-from .models import Classes, Teaching
+from .models import Classes, PrerequisiteClasses, Teaching
+
+
+# ====================================================================
+# Fields
+# ====================================================================
+
+class ClassChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.name
 
 
 # ====================================================================
@@ -48,6 +57,56 @@ class ClassInsertForm(forms.ModelForm):
         )
 
 
+class ClassDescriptionForm(forms.ModelForm):
+    class Meta:
+        model = Classes
+        fields = ["description"]
+        widgets = {
+            "description": forms.Textarea()
+        }
+
+
+# ====================================================================
+# Prerequisite classes
+# ====================================================================
+
+class PrerequisiteClassForm(forms.ModelForm):
+
+    def __init__(self, target_class, *args, **kwargs):
+        super(PrerequisiteClassForm, self).__init__(*args, **kwargs)
+        self.target_class = target_class
+        self.fields["prerequisite_id"] = ClassChoiceField(
+            queryset=Classes.objects.filter(
+                department=target_class.department
+            ).exclude(id=target_class.id),
+            required=True,
+            label=_("Prerequisite class")
+        )
+
+    class Meta:
+        model = PrerequisiteClasses
+        fields = ("prerequisite_id",)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        exists = PrerequisiteClasses.objects.filter(
+            class_id=self.target_class,
+            prerequisite_id=cleaned_data.get("prerequisite_id")
+        ).exists()
+
+        if exists:
+            raise ValidationError(
+                _("This prerequisite class has already been added."),
+                code="already_added")
+
+        if self.target_class == cleaned_data.get("prerequisite_id"):
+            raise ValidationError(
+                _("A class cannot be a prerequisite of itself."),
+                code="same_class")
+
+        return cleaned_data
+
+
 # ====================================================================
 # Teaching
 # ====================================================================
@@ -81,3 +140,40 @@ class TeachingInsertForm(forms.ModelForm):
         model = Teaching
         fields = ["year", "semester", "teacher", "theory_weight",
                   "lab_weight", "theory_limit", "lab_limit"]
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        theory_weight = cleaned_data.get("theory_weight")
+        lab_weight = cleaned_data.get("lab_weight")
+
+        if theory_weight is not None:
+            if theory_weight < 0 or theory_weight > 1:
+                raise ValidationError(
+                    _("The theory weight given is out of range."
+                      " Make sure it is a value between 0 and 1 inclusive."),
+                    code="weight_range_error")
+            if lab_weight is None:
+                lab_weight = Decimal(1) - theory_weight
+                cleaned_data["lab_weight"] = lab_weight
+            if theory_weight + lab_weight != 1:
+                raise ValidationError(
+                    _("Make sure the sum of the given weights is 1."),
+                    code="weight_sum_error"
+                )
+        elif lab_weight is not None:
+            if lab_weight < 0 or lab_weight > 1:
+                raise ValidationError(
+                    _("The lab weight given is out of range."
+                      " Make sure it is a value between 0 and 1 inclusive."),
+                    code="weight_range_error")
+            if theory_weight is None:
+                theory_weight = Decimal(1) - lab_weight
+                cleaned_data["theory_weight"] = theory_weight
+            if theory_weight + lab_weight != 1:
+                raise ValidationError(
+                    _("Make sure the sum of the given weights is 1."),
+                    code="weight_sum_error"
+                )
+
+        return cleaned_data
