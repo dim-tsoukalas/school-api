4b028542a6fe132406fcc0f476278987ddfb5a3a
sot-andreas <icsd18215@icsd.aegean.gr>
2021-08-15 20:56:28 +0300

    Added an is_teacher perm, the prerequisite classes add and delete views, and a teacher's teachings view.

--- src/classes/views.py
+++ src/classes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import PermissionDenied
 from django.http import Http404
+from django.db.models import Max
 
 from mainpage.views import init_render_dict
 from mainpage.models import Department
@@ -8,7 +9,10 @@ from mainpage.models import Department
 from users.models import Deptadmin, Teacher
 
 from .models import Classes, PrerequisiteClasses, Teaching, ClassSignup
-from .forms import ClassInsertForm, TeachingInsertForm
+from .forms import (
+    ClassInsertForm, ClassDescriptionForm, PrerequisiteClassForm,
+    TeachingInsertForm
+)
 
 
 # ====================================================================
@@ -28,7 +32,7 @@ def render_dict_add_dept(t_dict, dept_id):
     return t_dict
 
 
-def render_dict_add_perm(t_dict, request, dept_id, class_id=False):
+def render_dict_add_perm(t_dict, request, dept_id, class_public_id=False):
     user = request.user
 
     is_admin = False
@@ -40,6 +44,17 @@ def render_dict_add_perm(t_dict, request, dept_id, class_id=False):
         elif user.is_deptadmin:
             if Deptadmin.objects.get(user=user).department.id == dept_id:
                 is_admin = True
+        elif class_public_id and user.is_teacher:
+            try:
+                year = Teaching.objects.aggregate(Max("year"))["year__max"]
+                class_obj = Classes.objects.get(public_id=class_public_id)
+                teacher = Teacher.objects.get(user=user)
+                Teaching.objects.get(
+                    class_id=class_obj, teacher=teacher, year=year)
+                is_teacher = True
+            except (Classes.DoesNotExist, Teacher.DoesNotExist,
+                    Teaching.DoesNotExist):
+                pass
 
     t_dict.update({
         "perm": {
@@ -175,7 +190,7 @@ def details(request, dept_id, class_public_id):
     except Department.DoesNotExist:
         raise Http404(f"No department with id {dept_id}")
 
-    render_dict_add_perm(d, request, dept_id)
+    render_dict_add_perm(d, request, dept_id, class_public_id)
     render_dict_add_class(d, class_public_id)
 
     return render(request, "classes_details.html", d)
@@ -234,7 +249,7 @@ def info_update_get(request, dept_id, class_public_id):
     except Department.DoesNotExist:
         raise Http404(f"No department with id {dept_id}")
 
-    d = render_dict_add_perm(d, request, dept_id)
+    d = render_dict_add_perm(d, request, dept_id, class_public_id)
     d = render_dict_add_class(d, class_public_id)
 
     if not (d["perm"]["is_admin"] or d["perm"]["is_teacher"]):
@@ -242,7 +257,11 @@ def info_update_get(request, dept_id, class_public_id):
 
     dept_obj = d["dept"]["object"]
     class_obj = d["class"]["object"]
-    form = ClassInsertForm(dept_obj, instance=class_obj)
+    if d["perm"]["is_admin"]:
+        form = ClassInsertForm(dept_obj, instance=class_obj)
+    elif d["perm"]["is_teacher"]:
+        form = ClassDescriptionForm(instance=class_obj)
+
     d.update({
         "form": form,
         "t": {
@@ -259,7 +278,7 @@ def info_update_post(request, dept_id, class_public_id):
     except Department.DoesNotExist:
         raise Http404(f"No department with id {dept_id}")
 
-    d = render_dict_add_perm(d, request, dept_id)
+    d = render_dict_add_perm(d, request, dept_id, class_public_id)
     d = render_dict_add_class(d, class_public_id)
 
     if not (d["perm"]["is_admin"] or d["perm"]["is_teacher"]):
@@ -267,13 +286,19 @@ def info_update_post(request, dept_id, class_public_id):
 
     dept_obj = d["dept"]["object"]
     class_obj = d["class"]["object"]
-    form = ClassInsertForm(dept_obj, request.POST, instance=class_obj)
+    if d["perm"]["is_admin"]:
+        form = ClassInsertForm(dept_obj, request.POST, instance=class_obj)
+        new_public_id = form.cleaned_data["public_id"]  # Use the new id.
+    elif d["perm"]["is_teacher"]:
+        form = ClassDescriptionForm(request.POST, instance=class_obj)
+        new_public_id = class_public_id
+
     if form.is_valid():
         form.save()
         return redirect(
             "class",
             dept_id=dept_id,
-            class_public_id=form.cleaned_data["public_id"]  # Use the new id.
+            class_public_id=new_public_id
         )
     else:
         d.update({
@@ -284,6 +309,91 @@ def info_update_post(request, dept_id, class_public_id):
             }
         })
         return render(request, "base_form.html", d)
+
+
+# ====================================================================
+# /departments/<int:dept_id>/classes/<int:class_public_id>/prerequisites/insert
+# ====================================================================
+
+def prerequisites_insert(request, dept_id, class_public_id):
+    if request.method == "POST":
+        return prerequisites_insert_post(request, dept_id, class_public_id)
+
+    return prerequisites_insert_get(request, dept_id, class_public_id)
+
+
+def prerequisites_insert_get(request, dept_id, class_public_id):
+    d = render_dict_add_perm({}, request, dept_id, class_public_id)
+
+    if not (d["perm"]["is_admin"] or d["perm"]["is_teacher"]):
+        raise PermissionDenied
+
+    class_obj = Classes.objects.get(public_id=class_public_id)
+    form = PrerequisiteClassForm(
+        class_obj,
+        instance=PrerequisiteClasses(class_id=class_obj))
+    return render_base_form(
+        request, d, form, title="Add prerequisite class", submit_val="Add")
+
+
+def prerequisites_insert_post(request, dept_id, class_public_id):
+    d = render_dict_add_perm({}, request, dept_id, class_public_id)
+
+    if not (d["perm"]["is_admin"] or d["perm"]["is_teacher"]):
+        raise PermissionDenied
+
+    class_obj = Classes.objects.get(public_id=class_public_id)
+    form = PrerequisiteClassForm(
+        class_obj,
+        request.POST,
+        instance=PrerequisiteClasses(class_id=class_obj))
+    if form.is_valid():
+        form.save()
+        return redirect(
+            "class", dept_id=dept_id, class_public_id=class_public_id
+        )
+    else:
+        return render_base_form(
+            request, d, form, title="Add prerequisite class", submit_val="Add")
+
+
+# ====================================================================
+# /departments/<int:dept_id>/classes/<int:class_public_id>
+# /prerequisites/<int:req_id>/delete
+# ====================================================================
+
+def prerequisites_delete(request, dept_id, class_public_id, req_id):
+    if request.method == "POST":
+        return prerequisites_delete_post(
+            request, dept_id, class_public_id, req_id
+        )
+
+    return prerequisites_delete_get(request, dept_id, class_public_id, req_id)
+
+
+def prerequisites_delete_get(request, dept_id, class_public_id, req_id):
+    d = render_dict_add_perm({}, request, dept_id, class_public_id)
+
+    if not (d["perm"]["is_admin"] or d["perm"]["is_teacher"]):
+        raise PermissionDenied
+
+    d["t"] = {
+        "title": "Delete teaching?",
+        "confirm_text": ("Are you sure that you want to delete this"
+                         " prerequisite class?"),
+        "return_url": f"/departments/{dept_id}/classes/{class_public_id}"
+    }
+    return render(request, "confirm_form.html", d)
+
+
+def prerequisites_delete_post(request, dept_id, class_public_id, req_id):
+    d = render_dict_add_perm({}, request, dept_id, class_public_id)
+
+    if not (d["perm"]["is_admin"] or d["perm"]["is_teacher"]):
+        raise PermissionDenied
+
+    PrerequisiteClasses.objects.filter(id=req_id).delete()
+    return redirect("class", dept_id, class_public_id)
 
 
 # ====================================================================
@@ -345,7 +455,7 @@ def teaching_update(request, dept_id, class_public_id, teaching_id):
 
 
 def teaching_update_get(request, dept_id, class_public_id, teaching_id):
-    d = render_dict_add_perm({}, request, dept_id)
+    d = render_dict_add_perm({}, request, dept_id, class_public_id)
 
     if d["perm"]["is_admin"]:
         form_mode = "admin_update"
@@ -361,7 +471,7 @@ def teaching_update_get(request, dept_id, class_public_id, teaching_id):
 
 
 def teaching_update_post(request, dept_id, class_public_id, teaching_id):
-    d = render_dict_add_perm({}, request, dept_id)
+    d = render_dict_add_perm({}, request, dept_id, class_public_id)
 
     if d["perm"]["is_admin"]:
         form_mode = "admin_update"
@@ -425,3 +535,25 @@ def teaching_delete_post(request, dept_id, class_public_id, teaching_id):
 
     Teaching.objects.filter(id=teaching_id).delete()
     return redirect("class", dept_id, class_public_id)
+
+
+# ====================================================================
+# /users/<int:user_id>/classes
+# ====================================================================
+
+def user_classes(request, user_id):
+    d = init_render_dict(request)
+
+    if d["user"].get("is_teacher"):
+        return user_classes_teacher(request, d)
+
+    return Http404()
+
+
+def user_classes_teacher(request, d):
+    teacher = Teacher.objects.get(user=d["user"]["object"])
+    year = Teaching.objects.aggregate(Max("year"))["year__max"]
+    teachings = Teaching.objects.filter(
+        teacher=teacher, year=year).select_related("class_id")
+    d["teachings"] = teachings
+    return render(request, "classes_teacher.html", d)
