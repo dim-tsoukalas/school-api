728e47d3d70de547b7dfaf946720cbda940d4084
sot-andreas <icsd18215@icsd.aegean.gr>
2021-08-29 23:40:09 +0300

    init_render_dict now contains a university entry with the univerisy's name. It also contains a user.{teacher|student|deptadmin} entry that contains a list of their departments. render_dict_add_perm was moved here. Used the new template dicts for the base_form and confirm_form templates. Added views for changing the department state.

--- src/mainpage/views.py
+++ src/mainpage/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import PermissionDenied
 from django.http import Http404
+from django.db.models import Max
 
+from users.models import Deptadmin, Teacher
 from users.forms import SigninForm
 
-from .models import Department
-from .forms import DepartmentAddForm
+from classes.models import Classes, Teaching
+
+from .models import Department, DepartmentTeachers, DepartmentStudents
+from .forms import DepartmentAddForm, DepartmentStateForm
 
 
 # ====================================================================
@@ -13,25 +17,85 @@ from .forms import DepartmentAddForm
 # ====================================================================
 
 def init_render_dict(request):
-    t_dict = {
+    d = {
         "user": {
             "object": request.user,
             "is_superuser": request.user.is_superuser,
             "is_authenticated": request.user.is_authenticated
+        },
+        "university": {
+            "name": "Example University"
         }
     }
 
     if request.user.is_authenticated:
-        t_dict["user"].update({
+        d["user"].update({
             "id": request.user.id,
             "is_teacher": request.user.is_teacher,
             "is_student": request.user.is_student,
             "is_deptadmin": request.user.is_deptadmin
         })
+        if request.user.is_teacher:
+            d["user"].update({
+                "teacher": {
+                    "departments": DepartmentTeachers.objects.filter(
+                        user_id=request.user
+                    )
+                }
+            })
+        elif request.user.is_student:
+            d["user"].update({
+                "student": {
+                    "departments": DepartmentStudents.objects.filter(
+                        user_id=request.user
+                    )
+                }
+            })
+        elif request.user.is_deptadmin:
+            d["user"].update({
+                "deptadmin": {
+                    "department": Deptadmin.objects.get(
+                        user=request.user
+                    ).department
+                }
+            })
     else:
-        t_dict["user"]["signin_form"] = SigninForm()
+        d["user"]["signin_form"] = SigninForm()
 
-    return t_dict
+    return d
+
+
+def render_dict_add_perm(d, request, dept_id, class_public_id=False):
+    user = request.user
+
+    is_admin = False
+    is_teacher = False
+
+    if user.is_authenticated:
+        if user.is_superuser:
+            is_admin = True
+        elif user.is_deptadmin:
+            if Deptadmin.objects.get(user=user).department.id == dept_id:
+                is_admin = True
+        elif class_public_id and user.is_teacher:
+            try:
+                year = Teaching.objects.aggregate(Max("year"))["year__max"]
+                class_obj = Classes.objects.get(public_id=class_public_id)
+                teacher = Teacher.objects.get(user=user)
+                Teaching.objects.get(
+                    class_id=class_obj, teacher=teacher, year=year)
+                is_teacher = True
+            except (Classes.DoesNotExist, Teacher.DoesNotExist,
+                    Teaching.DoesNotExist):
+                pass
+
+    d.update({
+        "perm": {
+            "is_admin": is_admin,
+            "is_teacher": is_teacher
+        }
+    })
+    return d
 
 
 # ====================================================================
@@ -68,15 +132,13 @@ def departments_insert(request):
 
 
 def departments_insert_get(request, form=False):
-    t_dict = {
+    d = init_render_dict(request)
+    d["base_form"] = {
         "form": form if form else DepartmentAddForm(),
-        "t": {
-            "title": "Insert department",
-            "sumbit_value": "Insert"
-        }
+        "title": "Insert department",
+        "submit_value": "Insert"
     }
-
-    return render(request, "base_form.html", t_dict)
+    return render(request, "base_form.html", d)
 
 
 def departments_insert_post(request):
@@ -93,7 +155,10 @@ def departments_insert_post(request):
 # ====================================================================
 
 def departments_update(request, dept_id):
-    if not request.user.is_superuser:
+    d = init_render_dict(request)
+    d = render_dict_add_perm(d, request, dept_id)
+
+    if not d["perm"]["is_admin"]:
         raise PermissionDenied
 
     try:
@@ -102,30 +167,68 @@ def departments_update(request, dept_id):
         raise Http404(f"No department with id {dept_id}")
 
     if request.method == "POST":
-        return departments_update_post(request, dept_id, dept_obj)
+        return departments_update_post(request, d, dept_obj)
 
-    return departments_update_get(request, dept_id, dept_obj)
+    return departments_update_get(request, d, dept_obj)
 
 
-def departments_update_get(request, dept_id, dept_obj, form=False):
-    t_dict = {
+def departments_update_get(request, d, dept_obj, form=False):
+    d["base_form"] = {
         "form": form if form else DepartmentAddForm(instance=dept_obj),
-        "t": {
-            "title": "Update department",
-            "sumbit_value": "Update"
-        }
+        "title": "Update department",
+        "submit_value": "Update"
     }
+    return render(request, "base_form.html", d)
 
-    return render(request, "base_form.html", t_dict)
 
-
-def departments_update_post(request, dept_id, dept_obj):
+def departments_update_post(request, d, dept_obj):
     form = DepartmentAddForm(request.POST, instance=dept_obj)
     if form.is_valid():
         form.save()
         return redirect("departments")
     else:
-        return departments_update_get(request, dept_id, dept_obj, form=form)
+        return departments_update_get(request, d, dept_obj, form=form)
+
+
+# ====================================================================
+# /departments/<int:dept_id>/state
+# ====================================================================
+
+def departments_state(request, dept_id):
+    d = init_render_dict(request)
+    d = render_dict_add_perm(d, request, dept_id)
+
+    if not d["perm"]["is_admin"]:
+        raise PermissionDenied
+
+    try:
+        dept_obj = Department.objects.get(id=dept_id)
+    except Department.DoesNotExist:
+        raise Http404(f"No department with id {dept_id}")
+
+    if request.method == "POST":
+        return departments_state_post(request, d, dept_obj)
+
+    return departments_state_get(request, d, dept_obj)
+
+
+def departments_state_get(request, d, dept_obj, form=False):
+    d["base_form"] = {
+        "form": form if form else DepartmentStateForm(
+            dept_obj, instance=dept_obj),
+        "title": "Update department state",
+        "submit_value": "Save"
+    }
+    return render(request, "base_form.html", d)
+
+
+def departments_state_post(request, d, dept_obj):
+    form = DepartmentStateForm(dept_obj, request.POST, instance=dept_obj)
+    if form.is_valid():
+        form.save()
+        return redirect("departments")
+    else:
+        return departments_state_get(request, d, dept_obj, form=form)
 
 
 # ====================================================================
@@ -145,13 +248,11 @@ def departments_delete(request, dept_id):
         dept_obj.delete()
         return redirect("departments")
 
-    t_dict = {
-        "t": {
-            "title": f"Delete {dept_obj.name}",
-            "confirm_text": ("Are you sure that you want to delete the"
-                             f" department {dept_obj.name}?"),
-            "return_url": "/departments"
-        }
+    d = init_render_dict(request)
+    d["confirm_form"] = {
+        "title": f"Delete {dept_obj.name}",
+        "text": ("Are you sure that you want to delete the"
+                 f" department {dept_obj.name}?"),
+        "return_url": "/departments"
     }
-
-    return render(request, "confirm_form.html", t_dict)
+    return render(request, "confirm_form.html", d)
